6cd85ab3bec4fe4b4490666d57ef9db800954379
Ahmed Zaghloul <ahmed.zaghloul@robustastudio.com>
2019-05-05 13:46:24 +0200

    Initial Commit

---
+++ ClosureExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClosureExample
+//
+//  Created by Ahmed Zaghloul on 5/5/19.
+//  Copyright © 2019 Ahmed Zaghloul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
